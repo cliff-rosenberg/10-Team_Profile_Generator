6e99712862be5821074ea4d0d1e8f9240063e055
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-05-15 18:13:41 -0700

    added Inquirer functions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 // HTML generator
-const render = require('./src/htmlRender');
+const renderHtml = require('./src/htmlRender');
 
 // include classes here
 const Employee = require('./lib/Employee');
@@ -25,3 +25,75 @@ const OUTPUT_DIR = path.resolve(__dirname, "dist");
 // then normalizes the resulting path."
 const htmlOutputPath = path.join(OUTPUT_DIR, "teaminfo.html");
 //console.log(htmlOutputPath);
+
+// define global varaibles here
+const team = [];
+const idArray = [];
+
+// ask for team memners info, Manager first then others
+const userInquire = () => {
+        return inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'Enter Team Manager Name: ',
+                    name: 'managerName',
+                },
+                {
+                    type: 'input',
+                    message: 'Enter Team Manager Employee ID: ',
+                    name: 'managerId',
+                },
+                {
+                    type: 'input',
+                    message: 'Enter Team Manager Email: ',
+                    name: 'managerEmail',
+                },
+                {
+                    type: 'input',
+                    message: 'Enter Team Manager Office Number: ',
+                    name: 'managerOfficeNum',
+                },
+            ]).then((response) => {
+                    // assign input data to manager Object
+                    const teamManager = new Manager(response.managerId, response.managerName, response.managerEmail, response.managerOfficeNum);
+                    team.push(teamManager);
+                    idArray.push(response.managerId);
+                    // enter other team member data
+                    otherTeamInput();
+                })
+};// end inquirer function
+
+// ask to add other team members (Engineer or Intern)
+const otherTeamInput = () => {
+        return inquirer.prompt([
+                {
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ['Add Engineer','Add Intern','Done'],
+                name: 'input',
+                },
+            ]).then((response) => {
+                    if (response.input === 'Add Engineer') {
+                        addEngineer()
+                    }
+                    if (response.input === 'Add Intern') {
+                        addIntern()
+                    }
+                    if (response.input === 'Done') {
+                        writeToFile()
+                    }
+                })
+};// end inquirer function
+
+const addEngineer = (id,name,email,github) => {
+    
+};
+
+//init of app
+const init = () => {
+    userInquire();
+};
+// ****
+// start app here
+// ****
+init();
